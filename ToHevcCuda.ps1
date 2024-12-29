c2e8a53c76a40d8557852c94f83048fcf4e7f753
Maksym Nedvyha <imus663@gmail.com>
2024-12-29 12:00:31 +0200

    feat: add script for HEVC video conversion with CUDA support

---
+++ ToHevcCuda.ps1
@@ -0,0 +1,15 @@
+Param (
+    [Parameter(Position=0, Mandatory=$true)]
+    [string]
+    $InputFile,
+    [Parameter(Position=1)]
+    [int]
+    $Quality = 23
+)
+
+$OutputDirectory = 'output'
+$OutputFile = $OutputDirectory + '\' + $InputFile
+
+New-Item -ItemType Directory -Force $OutputDirectory -ErrorAction Stop
+
+ffmpeg -hwaccel cuda -i $InputFile -c:v hevc_nvenc -preset:v p7 -tune:v uhq -rc:v vbr_hq -cq:v $Quality -b:v 0 $OutputFile
