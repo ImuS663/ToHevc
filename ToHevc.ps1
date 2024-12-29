ffc7180c7e2a88a3b796e45122ce3a5613d6bfc8
Maksym Nedvyha <imus663@gmail.com>
2024-12-29 11:59:56 +0200

    feta: add script for HEVC video conversion

---
+++ ToHevc.ps1
@@ -0,0 +1,15 @@
+Param (
+    [Parameter(Position=0, Mandatory=$true)]
+    [string]
+    $InputFile,
+    [Parameter(Position=1)]
+    [int]
+    $Quality = 23
+)
+
+$OutputDirectory = 'output'
+$OutputFile = $OutputDirectory + '\' + $InputFile
+
+New-Item -ItemType Directory -Force $OutputDirectory -ErrorAction Stop
+
+ffmpeg -i $InputFile -c:v libx256 -preset:v slow -crf:v $Quality $OutputFile
