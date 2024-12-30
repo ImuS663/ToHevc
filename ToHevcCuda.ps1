e0e3b66f86d6510ea5347da879ede8fd547ea22e
Maksym Nedvyha <imus663@gmail.com>
2024-12-30 20:50:37 +0200

    feat: add  `-hwaccel_output_format cuda`

--- ToHevcCuda.ps1
+++ ToHevcCuda.ps1
@@ -12,4 +12,4 @@ $OutputFile = $OutputDirectory + '\' + $InputFile
 
 New-Item -ItemType Directory -Force $OutputDirectory -ErrorAction Stop
 
-ffmpeg -hwaccel cuda -i $InputFile -c:v hevc_nvenc -preset:v p7 -tune:v uhq -rc:v vbr -cq:v $Quality -b:v 0 $OutputFile
+ffmpeg -hwaccel cuda -hwaccel_output_format cuda -i $InputFile -c:v hevc_nvenc -preset:v p7 -tune:v uhq -rc:v vbr -cq:v $Quality -b:v 0 $OutputFile
