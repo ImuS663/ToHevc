5d391ffbf1a3cee31ce7b4a111324247163418f5
Maksym Nedvyha <imus663@gmail.com>
2024-12-31 13:56:07 +0200

    feat: update input file parameter to use System.IO.FileInfo and adjust output directory path

--- ToHevc.ps1
+++ ToHevc.ps1
@@ -1,15 +1,15 @@
 Param (
     [Parameter(Position=0, Mandatory=$true)]
-    [string]
+    [System.IO.FileInfo]
     $InputFile,
     [Parameter(Position=1)]
     [int]
     $Quality = 28
 )
 
-$OutputDirectory = 'output'
-$OutputFile = $OutputDirectory + '\' + $InputFile
+$OutputDirectory = $InputFile.Directory.FullName + '\output'
+$OutputFile = $OutputDirectory + '\' + $InputFile.Name
 
 New-Item -ItemType Directory -Force $OutputDirectory -ErrorAction Stop
 
-ffmpeg -i $InputFile -c:v libx256 -preset:v slow -crf:v $Quality $OutputFile
+ffmpeg -i $InputFile.FullName -c:v libx256 -preset:v slow -crf:v $Quality $OutputFile
--- ToHevcCuda.ps1
+++ ToHevcCuda.ps1
@@ -1,15 +1,15 @@
 Param (
     [Parameter(Position=0, Mandatory=$true)]
-    [string]
+    [System.IO.FileInfo]
     $InputFile,
     [Parameter(Position=1)]
     [int]
     $Quality = 28
 )
 
-$OutputDirectory = 'output'
-$OutputFile = $OutputDirectory + '\' + $InputFile
+$OutputDirectory = $InputFile.Directory.FullName + '\output'
+$OutputFile = $OutputDirectory + '\' + $InputFile.Name
 
 New-Item -ItemType Directory -Force $OutputDirectory -ErrorAction Stop
 
-ffmpeg -hwaccel cuda -hwaccel_output_format cuda -i $InputFile -c:v hevc_nvenc -preset:v p7 -tune:v uhq -rc:v vbr -cq:v $Quality -b:v 0 $OutputFile
+ffmpeg -hwaccel cuda -hwaccel_output_format cuda -i $InputFile.FullName -c:v hevc_nvenc -preset:v p7 -tune:v uhq -rc:v vbr -cq:v $Quality -b:v 0 $OutputFile
