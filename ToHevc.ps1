d7bc688bc9af83268b5a249ea10710f455cee59a
Maksym Nedvyha <imus663@gmail.com>
2024-12-31 14:50:56 +0200

    feat: add width and height parameters for output scaling

--- ToHevc.ps1
+++ ToHevc.ps1
@@ -4,7 +4,13 @@ Param (
     $InputFile,
     [Parameter(Position=1)]
     [int]
-    $Quality = 28
+    $Quality = 28,
+    [Parameter(Position=2)]
+    [int]
+    $WidthSize = -1,
+    [Parameter(Position=3)]
+    [int]
+    $HeightSize = -1
 )
 
 $OutputDirectory = $InputFile.Directory.FullName + '\output'
@@ -12,4 +18,8 @@ $OutputFile = $OutputDirectory + '\' + $InputFile.Name
 
 New-Item -ItemType Directory -Force $OutputDirectory -ErrorAction Stop
 
-ffmpeg -i $InputFile.FullName -c:v libx256 -preset:v slow -crf:v $Quality $OutputFile
+if (($WidthSize -eq -1) -and ($HeightSize -eq -1)) {
+    ffmpeg -i $InputFile.FullName -c:v libx256 -preset:v slow -crf:v $Quality $OutputFile
+} else {
+    ffmpeg -i $InputFile.FullName -c:v libx256 -vf scale=$('{0}:{1}' -f $WidthSize, $HeightSize) -preset:v slow -crf:v $Quality $OutputFile
+}
--- ToHevcCuda.ps1
+++ ToHevcCuda.ps1
@@ -4,7 +4,13 @@ Param (
     $InputFile,
     [Parameter(Position=1)]
     [int]
-    $Quality = 28
+    $Quality = 28,
+    [Parameter(Position=2)]
+    [int]
+    $WidthSize = -1,
+    [Parameter(Position=3)]
+    [int]
+    $HeightSize = -1
 )
 
 $OutputDirectory = $InputFile.Directory.FullName + '\output'
@@ -12,4 +18,8 @@ $OutputFile = $OutputDirectory + '\' + $InputFile.Name
 
 New-Item -ItemType Directory -Force $OutputDirectory -ErrorAction Stop
 
-ffmpeg -hwaccel cuda -hwaccel_output_format cuda -i $InputFile.FullName -c:v hevc_nvenc -preset:v p7 -tune:v uhq -rc:v vbr -cq:v $Quality -b:v 0 $OutputFile
+if (($WidthSize -eq -1) -and ($HeightSize -eq -1)) {
+    ffmpeg -hwaccel cuda -hwaccel_output_format cuda -i $InputFile.FullName -c:v hevc_nvenc -preset:v p7 -tune:v uhq -rc:v vbr -cq:v $Quality -b:v 0 $OutputFile
+} else {
+    ffmpeg -hwaccel cuda -hwaccel_output_format cuda -i $InputFile.FullName -c:v hevc_nvenc -vf scale_cuda=$('{0}:{1}' -f $WidthSize, $HeightSize) -preset:v p7 -tune:v uhq -rc:v vbr -cq:v $Quality -b:v 0 $OutputFile
+}
