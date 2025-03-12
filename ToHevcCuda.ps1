c8c538c9b7426452c53f469a5e1afe90a9e01b43
Maksym Nedvyha <imus663@gmail.com>
2025-03-12 21:03:59 +0200

    feat: enhance ToHevcCuda.ps1 with native decode option and remove ToHevcCudaNatDecode.ps1

--- ToHevcCuda.ps1
+++ ToHevcCuda.ps1
@@ -10,7 +10,10 @@ Param (
     $WidthSize = -1,
     [Parameter(Position=3)]
     [int]
-    $HeightSize = -1
+    $HeightSize = -1,
+    [Parameter(Position=4)]
+    [switch]
+    $NativeDecode = $false
 )
 
 $OutputDirectory = $InputFile.Directory.FullName + "\output"
@@ -18,8 +21,12 @@ $OutputFile = $OutputDirectory + "\" + $InputFile.BaseName + ".mp4"
 
 New-Item -ItemType Directory -Force $OutputDirectory -ErrorAction Stop
 
-$scale = !(($WidthSize -eq -1) -and ($HeightSize -eq -1)) ? "-vf scale_cuda=$($WidthSize):$($HeightSize)" : ""
+$scaleType = $NativeDecode ? "scale" : "scale_cuda"
 
-$ffmpegCommand = "ffmpeg -hwaccel cuda -hwaccel_output_format cuda -i $($InputFile.FullName) -c:v hevc_nvenc $scale -preset:v p7 -tune:v uhq -rc:v vbr -cq:v $Quality -b:v 0 $OutputFile"
+$scale = !(($WidthSize -eq -1) -and ($HeightSize -eq -1)) ? "-vf $scaleType=$($WidthSize):$($HeightSize)" : ""
+
+$hwaccel = !$NativeDecode ? "-hwaccel cuda" : ""
+
+$ffmpegCommand = "ffmpeg $hwaccel -hwaccel_output_format cuda -i $($InputFile.FullName) -c:v hevc_nvenc $scale -preset:v p7 -tune:v uhq -rc:v vbr -cq:v $Quality -b:v 0 $OutputFile"
 
 Invoke-Expression $ffmpegCommand
