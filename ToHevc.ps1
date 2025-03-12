0201b1b68a91260232b1af6d43c95ec0e000d103
Maksym Nedvyha <imus663@gmail.com>
2025-03-12 20:27:26 +0200

    style: standardize string interpolation syntax

--- ToHevc.ps1
+++ ToHevc.ps1
@@ -13,13 +13,13 @@ Param (
     $HeightSize = -1
 )
 
-$OutputDirectory = $InputFile.Directory.FullName + '\output'
-$OutputFile = $OutputDirectory + '\' + $InputFile.BaseName + ".mp4"
+$OutputDirectory = $InputFile.Directory.FullName + "\output"
+$OutputFile = $OutputDirectory + "\" + $InputFile.BaseName + ".mp4"
 
 New-Item -ItemType Directory -Force $OutputDirectory -ErrorAction Stop
 
 if (($WidthSize -eq -1) -and ($HeightSize -eq -1)) {
     ffmpeg -i $InputFile.FullName -c:v libx265 -preset:v slow -crf:v $Quality $OutputFile
 } else {
-    ffmpeg -i $InputFile.FullName -c:v libx265 -vf scale=$('{0}:{1}' -f $WidthSize, $HeightSize) -preset:v slow -crf:v $Quality $OutputFile
+    ffmpeg -i $InputFile.FullName -c:v libx265 -vf scale=$("{0}:{1}" -f $WidthSize, $HeightSize) -preset:v slow -crf:v $Quality $OutputFile
 }
--- ToHevcCudaNatDecode.ps1
+++ ToHevcCudaNatDecode.ps1
@@ -13,13 +13,13 @@ Param (
     $HeightSize = -1
 )
 
-$OutputDirectory = $InputFile.Directory.FullName + '\output'
-$OutputFile = $OutputDirectory + '\' + $InputFile.BaseName + ".mp4"
+$OutputDirectory = $InputFile.Directory.FullName + "\output"
+$OutputFile = $OutputDirectory + "\" + $InputFile.BaseName + ".mp4"
 
 New-Item -ItemType Directory -Force $OutputDirectory -ErrorAction Stop
 
 if (($WidthSize -eq -1) -and ($HeightSize -eq -1)) {
     ffmpeg -hwaccel_output_format cuda -i $InputFile.FullName -c:v hevc_nvenc -preset:v p7 -tune:v uhq -rc:v vbr -cq:v $Quality -b:v 0 $OutputFile
 } else {
-    ffmpeg -hwaccel_output_format cuda -i $InputFile.FullName -c:v hevc_nvenc -vf scale=$('{0}:{1}' -f $WidthSize, $HeightSize) -preset:v p7 -tune:v uhq -rc:v vbr -cq:v $Quality -b:v 0 $OutputFile
+    ffmpeg -hwaccel_output_format cuda -i $InputFile.FullName -c:v hevc_nvenc -vf scale=$("{0}:{1}" -f $WidthSize, $HeightSize) -preset:v p7 -tune:v uhq -rc:v vbr -cq:v $Quality -b:v 0 $OutputFile
 }
